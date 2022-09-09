d3ed0a5b3ce3403ffd31b27732472b39e3bbbfa6
Yo Higashida <y.higashida0226+app@gmail.com>
2022-09-09 16:46:56 +0900

    add: タイマーを追加

--- lib/ui/component/overview_route.dart
+++ lib/ui/component/overview_route.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:trigger/model/recommended_overview_route.dart';
 import 'package:trigger/ui/component/use_route_facilities.dart';
@@ -14,14 +15,66 @@ class OverviewRoute extends StatefulWidget {
 }
 
 class _OverviewRouteState extends State<OverviewRoute> {
-  /// 破線単体の幅
-  final double dashedWidth = 5;
+  late Timer timer;
 
-  /// 破線の高さ
-  final double dashedHeight = 0.5;
+  String timeLeft = ''; // 残り時間
+  String unit = ''; // 残り時間の単位
+  bool isShowDepartureTime = true;
 
-  /// 破線の色
-  final Color color = Colors.black45;
+  final double dashedWidth = 5; // 破線単体の幅
+  final double dashedHeight = 0.5; // 破線の高さ
+  final Color color = Colors.black45; // 破線の色
+
+  @override
+  void initState() {
+    super.initState();
+    updateTimeLeft();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    stopTimer();
+  }
+
+  void startTimer() {
+    const oneMin = Duration(seconds: 1);
+    timer = Timer.periodic(oneMin, (timer) => updateTimeLeft());
+  }
+
+  void stopTimer() {
+    timer.cancel();
+  }
+
+  void updateTimeLeft() {
+    if (widget.route.isEnableTimeLimit && widget.route.timeLimit != null) {
+      final now = DateTime.now();
+      final diff = widget.route.timeLimit!.difference(now);
+
+      setState(() {
+        if (diff.inHours > 0) {
+          timeLeft = diff.inHours.toString().padLeft(2, '0');
+          if (diff.inHours == 1) {
+            unit = 'hr.';
+          } else {
+            unit = 'hrs.';
+          }
+        } else if (diff.inMinutes > 0) {
+          timeLeft = diff.inMinutes.toString().padLeft(2, '0');
+          unit = 'min.';
+        }
+
+        isShowDepartureTime = true;
+      });
+    } else {
+      setState(() {
+        timeLeft = '∞';
+        unit = 'min.';
+        isShowDepartureTime = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,20 +110,20 @@ class _OverviewRouteState extends State<OverviewRoute> {
                     children: [
                       const Text('出発まで'),
                       const SizedBox(height: 12),
-                      const Text.rich(
+                      Text.rich(
                         TextSpan(
                           children: [
                             TextSpan(
-                              text: '10',
-                              style: TextStyle(
+                              text: timeLeft,
+                              style: const TextStyle(
                                 fontSize: FontSize.pt60,
                                 fontWeight: FontWeight.w600,
                                 height: 0.8,
                               ),
                             ),
                             TextSpan(
-                              text: 'min',
-                              style: TextStyle(
+                              text: unit,
+                              style: const TextStyle(
                                 fontSize: FontSize.pt20,
                                 fontWeight: FontWeight.w600,
                               ),
@@ -78,10 +131,14 @@ class _OverviewRouteState extends State<OverviewRoute> {
                           ],
                         ),
                       ),
-                      Text(
-                        '出発時間: ${widget.route.getTimeLimit()}',
-                        style: TextStyle(color: Theme.of(context).primaryColor),
-                      ),
+                      isShowDepartureTime
+                          ? Text(
+                              '出発時間: ${widget.route.getTimeLimit()}',
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            )
+                          : const SizedBox(),
                     ],
                   ),
                   // 経路上でなにをつかうか
