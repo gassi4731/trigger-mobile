c9d9d0338340553f9760fa4cc1afa64f2ebb5e21
Yo Higashida <y.higashida0226+app@gmail.com>
2022-09-09 13:16:47 +0900

    add: mock-recommended_overview_route

---
+++ lib/mock/recommended_overview_route.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:trigger/model/recommended_overview_route.dart';
+
+RecommendedOverviewRoute mockRecommendedOverviewRoute([int length = 1]) {
+  final mocks = [
+    RecommendedOverviewRoute(
+      timeLimit: DateTime(2020, 10, 2, 12, 10),
+      isEnableTimeLimit: true,
+      amount: Random().nextInt(5000),
+      allPoint: Random().nextDouble() * 100,
+      physicalPoint: Random().nextDouble() * 100,
+      isUseTrain: true,
+      isUseTaxi: false,
+      isUseHotel: false,
+    ),
+    RecommendedOverviewRoute(
+      isEnableTimeLimit: false,
+      amount: Random().nextInt(5000),
+      allPoint: Random().nextDouble() * 100,
+      physicalPoint: Random().nextDouble() * 100,
+      isUseTrain: false,
+      isUseTaxi: true,
+      isUseHotel: false,
+    ),
+    RecommendedOverviewRoute(
+      timeLimit: DateTime(2020, 10, 2, 15, 10),
+      isEnableTimeLimit: true,
+      amount: Random().nextInt(5000),
+      allPoint: Random().nextDouble() * 100,
+      physicalPoint: Random().nextDouble() * 100,
+      isUseTrain: true,
+      isUseTaxi: true,
+      isUseHotel: false,
+    ),
+    RecommendedOverviewRoute(
+      timeLimit: DateTime(2020, 10, 2, 16, 10),
+      isEnableTimeLimit: true,
+      amount: Random().nextInt(5000),
+      allPoint: Random().nextDouble() * 100,
+      physicalPoint: Random().nextDouble() * 100,
+      isUseTrain: true,
+      isUseTaxi: false,
+      isUseHotel: true,
+    ),
+    RecommendedOverviewRoute(
+      isEnableTimeLimit: false,
+      amount: Random().nextInt(5000),
+      allPoint: Random().nextDouble() * 100,
+      physicalPoint: Random().nextDouble() * 100,
+      isUseTrain: false,
+      isUseTaxi: true,
+      isUseHotel: true,
+    ),
+    RecommendedOverviewRoute(
+      isEnableTimeLimit: false,
+      amount: Random().nextInt(5000),
+      allPoint: Random().nextDouble() * 100,
+      physicalPoint: Random().nextDouble() * 100,
+      isUseTrain: false,
+      isUseTaxi: false,
+      isUseHotel: true,
+    ),
+  ];
+
+  return mocks[0];
+}
