b9f9e8b12616a7c02ab9ccb397f90cb2b0734923
Yo Higashida <y.higashida0226+app@gmail.com>
2022-09-12 11:54:15 +0900

    fix: mock

--- lib/mock/recommended_overview_route.dart
+++ lib/mock/recommended_overview_route.dart
@@ -1,72 +1,19 @@
-import 'dart:math';
+import 'dart:convert';
 
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:trigger/model/recommended_route.dart';
 
-List<RecommendedOverviewRoute> mockRecommendedOverviewRoute([int length = 1]) {
-  final now = DateTime.now();
-  final year = now.year;
-  final month = now.month;
-  final day = now.day;
+Future<List<RecommendedRoute>> mockRecommendedRoutes() async {
+  final converted = <RecommendedRoute>[];
 
-  final mocks = [
-    RecommendedOverviewRoute(
-      timeLimit: DateTime(year, month, day + 1, 1, 10),
-      isEnableTimeLimit: true,
-      amount: Random().nextInt(500),
-      allPoint: Random().nextDouble() * 100,
-      physicalPoint: Random().nextDouble() * 100,
-      isUseTrain: true,
-      isUseTaxi: false,
-      isUseHotel: false,
-    ),
-    RecommendedOverviewRoute(
-      isEnableTimeLimit: false,
-      amount: Random().nextInt(5000),
-      allPoint: Random().nextDouble() * 100,
-      physicalPoint: Random().nextDouble() * 100,
-      isUseTrain: false,
-      isUseTaxi: true,
-      isUseHotel: false,
-    ),
-    RecommendedOverviewRoute(
-      timeLimit: DateTime(year, month, day, 16, 50),
-      isEnableTimeLimit: true,
-      amount: Random().nextInt(5000),
-      allPoint: Random().nextDouble() * 100,
-      physicalPoint: Random().nextDouble() * 100,
-      isUseTrain: true,
-      isUseTaxi: true,
-      isUseHotel: false,
-    ),
-    RecommendedOverviewRoute(
-      timeLimit: DateTime(year, month, day, 23, 10),
-      isEnableTimeLimit: true,
-      amount: Random().nextInt(5000),
-      allPoint: Random().nextDouble() * 100,
-      physicalPoint: Random().nextDouble() * 100,
-      isUseTrain: true,
-      isUseTaxi: false,
-      isUseHotel: true,
-    ),
-    RecommendedOverviewRoute(
-      isEnableTimeLimit: false,
-      amount: Random().nextInt(5000),
-      allPoint: Random().nextDouble() * 100,
-      physicalPoint: Random().nextDouble() * 100,
-      isUseTrain: false,
-      isUseTaxi: true,
-      isUseHotel: true,
-    ),
-    RecommendedOverviewRoute(
-      isEnableTimeLimit: false,
-      amount: Random().nextInt(5000),
-      allPoint: Random().nextDouble() * 100,
-      physicalPoint: Random().nextDouble() * 100,
-      isUseTrain: false,
-      isUseTaxi: false,
-      isUseHotel: true,
-    ),
-  ];
+  final loadData = await rootBundle.loadString('assets/json/mock_routes.json');
+  final jsonResponse = jsonDecode(loadData) as List<dynamic>;
+  final jsonResponseMap =
+      jsonResponse.map((dynamic e) => e as Map<dynamic, dynamic>);
 
-  return mocks;
+  for (final i in jsonResponseMap) {
+    converted.add(RecommendedRoute.fromJson(i));
+  }
+
+  return converted;
 }
