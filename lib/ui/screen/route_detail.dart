8ae863585ddd800ce8a521578911c7bb70312281
Yo Higashida <y.higashida0226+app@gmail.com>
2022-09-12 15:21:44 +0900

    add: mock

--- lib/ui/screen/route_detail.dart
+++ lib/ui/screen/route_detail.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:trigger/mock/recommended_overview_route.dart';
 import 'package:trigger/model/recommended_route_detail.dart';
+import 'package:trigger/ui/component/spot_route.dart';
 
 class RouteDetail extends StatefulWidget {
-  const RouteDetail({Key? key, required this.detail}) : super(key: key);
+  const RouteDetail({Key? key}) : super(key: key);
+  // const RouteDetail({Key? key, required this.detail}) : super(key: key);
 
-  final RecommendedRouteDetail detail;
+  // final RecommendedRouteDetail detail;
 
   @override
   State<RouteDetail> createState() => _RouteDetailState();
 }
 
 class _RouteDetailState extends State<RouteDetail> {
+  final details = <RecommendedRouteDetail>[];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMockDetail();
+  }
+
+  Future<void> fetchMockDetail() async {
+    details.addAll(await mockRecommendedRouteDetails());
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ルートの詳細')),
       body: SingleChildScrollView(
-          child: Column(
-        children: const [],
-      )),
+        child: Column(
+          children: [
+            details.isNotEmpty
+                ? SpotRoute(
+                    detail: details[0],
+                    afterDetail: details[1],
+                  )
+                : const SizedBox()
+          ],
+        ),
+      ),
     );
   }
 }
