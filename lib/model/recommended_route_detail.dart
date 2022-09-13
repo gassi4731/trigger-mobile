e04f2e9bd583a182e1dcd2b54f85275c959912a9
Yo Higashida <y.higashida0226+app@gmail.com>
2022-09-13 10:43:07 +0900

    fix: 新しいレスポンスに対応

--- lib/model/recommended_route_detail.dart
+++ lib/model/recommended_route_detail.dart
@@ -1,45 +1,36 @@
-import 'package:trigger/model/recommended_route_detail_train.dart';
+import 'package:trigger/model/recommended_route_next_action.dart';
 
 class RecommendedRouteDetail {
   RecommendedRouteDetail({
-    required this.from,
-    required this.to,
-    required this.leaveAt,
-    required this.arriveAt,
-    required this.requiredMinute,
+    required this.name,
+    required this.nextAction,
+    this.arriveAt,
+    this.leaveAt,
     required this.price,
-    required this.method,
-    required this.train,
   });
 
   factory RecommendedRouteDetail.fromJson(Map<dynamic, dynamic> json) {
-    final leaveAt = json['leave_at'] != ''
+    final leaveAt = json['leave_at'] != null
         ? DateTime.parse(json['leave_at'].toString())
         : null;
-    final arriveAt = json['arrive_at'] != ''
+    final arriveAt = json['arrive_at'] != null
         ? DateTime.parse(json['arrive_at'].toString())
         : null;
 
     return RecommendedRouteDetail(
-      from: json['from'].toString(),
-      to: json['to'].toString(),
-      leaveAt: leaveAt,
-      arriveAt: arriveAt,
-      requiredMinute: int.parse(json['required_minute'].toString()),
-      price: int.parse(json['price'].toString()),
-      method: json['method'].toString(),
-      train: RecommendedRouteDetailTrain.fromJson(
-        json['train'] as Map<dynamic, dynamic>,
+      name: json['name'].toString(),
+      nextAction: RecommendedRouteNextAction.fromJson(
+        json['next_action'] as Map<dynamic, dynamic>,
       ),
+      arriveAt: arriveAt,
+      leaveAt: leaveAt,
+      price: int.parse(json['price'].toString()),
     );
   }
 
-  final String from;
-  final String to;
-  final DateTime? leaveAt;
+  final String name;
+  final RecommendedRouteNextAction nextAction;
   final DateTime? arriveAt;
-  final int requiredMinute;
+  final DateTime? leaveAt;
   final int price;
-  final String method;
-  final RecommendedRouteDetailTrain train;
 }
