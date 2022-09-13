d10cae65e223ac75dd9f72c0cdfdc6c38feb048a
Yo Higashida <y.higashida0226+app@gmail.com>
2022-09-13 11:43:10 +0900

    wip: 現在地を取得するロジック周り

---
+++ lib/util/position.dart
@@ -0,0 +1,39 @@
+// import 'package:geolocator/geolocator.dart';
+
+// /// デバイスの現在位置を決定する。
+// /// 位置情報サービスが有効でない場合、または許可されていない場合。
+// /// エラーを返します
+// Future<Position> _determinePosition() async {
+//   bool serviceEnabled;
+//   GeolocationPermission permission;
+
+//   // 位置情報サービスが有効かどうかをテストします。
+//   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//   if (!serviceEnabled) {
+//     // 位置情報サービスが有効でない場合、続行できません。
+//     // 位置情報にアクセスし、ユーザーに対して
+//     // 位置情報サービスを有効にするようアプリに要請する。
+//     return Future.error('Location services are disabled.');
+//   }
+
+//   permission = await Geolocator.checkPermission();
+//   if (permission == GeolocationPermission.denied) {
+//     // ユーザーに位置情報を許可してもらうよう促す
+//     permission = await Geolocator.requestPermission();
+//     if (permission == GeolocationPermission.denied) {
+//       // 拒否された場合エラーを返す
+//       return Future.error('Location permissions are denied');
+//     }
+//   }
+
+//   // 永久に拒否されている場合のエラーを返す
+//   if (permission == LocationPermission.deniedForever) {
+//     return Future.error(
+//       'Location permissions are permanently denied, we cannot request permissions.',
+//     );
+//   }
+
+//   // ここまでたどり着くと、位置情報に対しての権限が許可されているということなので
+//   // デバイスの位置情報を返す。
+//   return await Geolocator.getCurrentPosition();
+// }
